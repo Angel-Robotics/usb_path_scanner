e9f4545b64fd832e2878ebbcaf70e04d5541859f
JAICHANG.PARK <jaichang@angel-robotics.com>
2021-04-22 12:00:30 +0900

    [Update] change sdk 30 to 29

--- lib/main.dart
+++ lib/main.dart
@@ -39,6 +39,11 @@ class _MyHomePageState extends State<MyHomePage> {
       _counter++;
     });
   }
+  void refreshCounter() {
+    setState(() {
+      _counter = 0;
+    });
+  }
 
   List<String> pathItems = [];
   List<FileSystemEntity> storageItems = [];
@@ -78,44 +83,41 @@ class _MyHomePageState extends State<MyHomePage> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    Permission.storage.request();
     refreshPath();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            ListTile(
+              title: Text("저장소권한"),
+              onTap: () async {
+                var status = await Permission.storage.status;
+                if (!status.isGranted) {
+                  await Permission.storage.request();
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("${status}")));
+                }
+                // Permission.storage.request();
+              },
+              onLongPress: () async {
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("${await Permission.storage.status}")));
+              },
+            ),
+            Divider(),
+            ListTile(
+              title: Text("manageExternalStorage"),
+              onTap: () {
+                Permission.manageExternalStorage.request();
+              },
+            ),
             Divider(),
             IconButton(
                 icon: Icon(Icons.refresh),
@@ -123,41 +125,220 @@ class _MyHomePageState extends State<MyHomePage> {
                   refreshPath();
                 }),
             Divider(),
+
             Padding(
               padding: const EdgeInsets.all(16.0),
               child: IconButton(
                   icon: Icon(Icons.check_circle_outline),
-                  onPressed: () async{
+                  onPressed: () async {
+                    pathItems.clear();
+                    // List<UsbDevice> devices = await UsbSerial.listDevices();
+                    //
+                    // print(devices);
+                    int idx = 0;
                     if (storageItems.length > 2) {
-                      print(storageItems[0].path);
+                      print(storageItems[idx].path);
                       pathItems.add("----------------------");
-                      pathItems.add("${storageItems[0]}");
-                      pathItems.add("path: ${storageItems[0].path}");
-                      pathItems.add("absolute: ${storageItems[0].absolute}");
-                      pathItems.add("uri: ${storageItems[0].uri}");
-
-                      Directory topPath = Directory(join(storageItems[0].path, "/Angel Legs/Report"));
-                      print("topPath: ${topPath}");
+                      // pathItems.add("${devices.toString()}");
                       pathItems.add("----------------------");
-                      pathItems.add("${topPath.path}");
-                      setState(() {
-                      });
+                      pathItems.add("${storageItems[idx]}");
+                      pathItems.add("path: ${storageItems[idx].path}");
+                      pathItems.add("absolute: ${storageItems[idx].absolute}");
+                      pathItems.add("uri: ${storageItems[idx].uri}");
 
-                      if(!await topPath.exists()) await topPath.create();
+                      pathItems.add("${join(storageItems[idx].path, "AngelLegsTest/")}");
+                      Directory topPath = Directory(join(storageItems[idx].path, "AngelLegsTest/"));
+                      print("topPath: $topPath");
+                      pathItems.add("----------------------");
+                      pathItems.add("topPath: ${topPath.path}");
+                      setState(() {});
+                      try {
+                        if (!await topPath.exists()) {
+                          await topPath.create();
 
-                      Directory tempPath = Directory(join(storageItems[0].path, "/Angel Legs/Report/Hello_${DateTime.now()}.txt"));
-                      try{
-                        File f = File(tempPath.path);
-                        await f.writeAsString("Hello World");
-                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Done!")));
-                      }catch(e){
+                          pathItems.add("topPath: Created");
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Create!")));
+                        } else {
+                          pathItems.add("topPath: already exist");
+                        }
+                      } catch (e) {
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("${e.toString()}")));
+                        pathItems.add("${e.toString()}");
+                      }
+
+                      Directory childPath = Directory(join(topPath.path, "Report/"));
+                      print("childPath: $childPath");
+                      pathItems.add("----------------------");
+                      pathItems.add("child: ${childPath.path}");
+
+                      try {
+                        if (!await childPath.exists()) {
+                          await childPath.create();
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Create!")));
+                          pathItems.add("childPath: Created");
+                        } else {
+                          pathItems.add("childPath: already exist");
+                        }
+                      } catch (e) {
+                        pathItems.add("${e.toString()}");
                         ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("${e.toString()}")));
                       }
 
-                    }else{
+                      Directory tempPath = Directory(join(childPath.path,
+                          "Hello_${DateTime.now().year}${DateTime.now().month}${DateTime.now().day}_${DateTime.now().millisecond}.txt"));
+
+                      try {
+                        File f = File(tempPath.path);
+                        await f.writeAsString("Hello World");
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Done!")));
+                      } catch (e) {
+                        pathItems.add("${e.toString()}");
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("${e.toString()}")));
+                      }
+                    } else {
                       ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("발견된 외부 장치가 없습니다.")));
                     }
+                    setState(() {});
                   }),
+            ),
+            ListTile(
+              title: Text("폴더 읽기 "),
+              trailing: Text("$_counter"),
+              onTap: () async {
+                pathItems.clear();
+                if (storageItems.length > 2) {
+                  print(storageItems[_counter].path);
+                  pathItems.add("----------------------");
+                  // pathItems.add("${devices.toString()}");
+                  pathItems.add("----------------------");
+                  pathItems.add("${storageItems[_counter]}");
+                  pathItems.add("path: ${storageItems[_counter].path}");
+                  pathItems.add("absolute: ${storageItems[_counter].absolute}");
+                  pathItems.add("uri: ${storageItems[_counter].uri}");
+
+                  try {
+                    Directory tempPath = Directory(storageItems[_counter].path);
+                     tempPath.listSync().forEach((element) {
+                       pathItems.add(element.path);
+                     });
+
+
+
+                  } catch (e) {
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("${e.toString()}")));
+                    pathItems.add("${e.toString()}");
+                  }
+                  setState(() {});
+                }
+              },
+            ),
+            ListTile(
+              title: Text("폴더만들기 1 "),
+              trailing: Text("$_counter"),
+              onTap: () async {
+                pathItems.clear();
+                if (storageItems.length > 2) {
+                  print(storageItems[_counter].path);
+                  pathItems.add("----------------------");
+                  // pathItems.add("${devices.toString()}");
+                  pathItems.add("----------------------");
+                  pathItems.add("${storageItems[_counter]}");
+                  pathItems.add("path: ${storageItems[_counter].path}");
+                  pathItems.add("absolute: ${storageItems[_counter].absolute}");
+                  pathItems.add("uri: ${storageItems[_counter].uri}");
+
+                  pathItems.add("${join(storageItems[_counter].path, "AngelLegsTest/")}");
+                  Directory topPath = Directory(join(storageItems[_counter].path, "AngelLegsTest/"));
+                  print("topPath: $topPath");
+                  pathItems.add("----------------------");
+                  pathItems.add("topPath: ${topPath.path}");
+                  try {
+                    if (!await topPath.exists()) {
+                      await topPath.create();
+
+                      pathItems.add("topPath: Created");
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Create!")));
+                    } else {
+                      pathItems.add("topPath: already exist");
+                    }
+
+                  } catch (e) {
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("${e.toString()}")));
+                    pathItems.add("${e.toString()}");
+                  }
+                  setState(() {});
+                }
+              },
+            ),
+            ListTile(
+              title: Text("파일쓰기"),
+              trailing: Text("$_counter"),
+              onTap: () async {
+                pathItems.clear();
+                Directory d =  await getExternalStorageDirectory();
+                File df = File(join(d.path, "hello.txt"));
+                try {
+                  await df.writeAsString("helldjfsl");
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("completed write ")));
+                } catch (e) {
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("${e.toString()}")));
+                  pathItems.add("${e.toString()}");
+                }
+
+                if (storageItems.length > 2) {
+                  print(storageItems[_counter].path);
+                  pathItems.add("----------------------");
+                  pathItems.add("${storageItems[_counter]}");
+                  pathItems.add("path: ${storageItems[_counter].path}");
+                  pathItems.add("absolute: ${storageItems[_counter].absolute}");
+                  pathItems.add("uri: ${storageItems[_counter].uri}");
+
+                  pathItems.add("${join(storageItems[_counter].path, "AngelLegsTest/")}");
+                  Directory topPath = Directory(join(storageItems[_counter].path, "AngelLegsTest/"));
+                  print("topPath: $topPath");
+                  pathItems.add("----------------------");
+                  pathItems.add("topPath: ${topPath.path}");
+                  try {
+                    Directory tempPath = Directory(join(storageItems[_counter].path,
+                        "Hello_${DateTime.now().year}${DateTime.now().month}${DateTime.now().day}_${DateTime.now().millisecond}.txt"));
+                    File f = File(tempPath.path);
+                    await f.writeAsString("Hello");
+                  } catch (e) {
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("${e.toString()}")));
+                    pathItems.add("${e.toString()}");
+                  }
+                  setState(() {});
+                }
+              },
+            ),
+            ListTile(
+              title: Text("파일 복사하기"),
+              trailing: Text("$_counter"),
+              onTap: () async {
+                pathItems.clear();
+                Directory d =  await getExternalStorageDirectory();
+                File df = File(join(d.path, "hello.txt"));
+                try {
+                  await df.writeAsString("helldjfsl");
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("completed write")));
+                } catch (e) {
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("${e.toString()}")));
+                  pathItems.add("${e.toString()}");
+                }
+
+                if (storageItems.length > 2) {
+                  print(storageItems[_counter].path);
+                  try {
+                    Directory tempPath = Directory(storageItems[_counter].path);
+                    File f = File(tempPath.path);
+                    df.copy(f.path);
+                  } catch (e) {
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("${e.toString()}")));
+                    pathItems.add("${e.toString()}");
+                  }
+                  setState(() {});
+                }
+              },
             ),
             Divider(),
             Expanded(
@@ -172,10 +353,20 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+      floatingActionButton: Row(
+        children: [
+          FloatingActionButton(
+            onPressed: _incrementCounter,
+            tooltip: 'Increment',
+            child: Icon(Icons.add),
+          ),
+          FloatingActionButton(
+            heroTag: "2",
+            onPressed: refreshCounter,
+            tooltip: 'Increment',
+            child: Icon(Icons.refresh),
+          ),
+        ],
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
